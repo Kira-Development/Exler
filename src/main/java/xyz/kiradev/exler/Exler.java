f86c5479e3ecf1e76548af942285bc7054c52add
Vifez <vifez01@gmail.com>
2024-07-07 20:14:26 +0100

    [+] Removed Clash API | I didnt test btw

--- src/main/java/xyz/kiradev/exler/Exler.java
+++ src/main/java/xyz/kiradev/exler/Exler.java
@@ -1,26 +1,53 @@
 package xyz.kiradev.exler;
 
+/*
+ *
+ * Exler is a property of Kira-Development-Team
+ * It was created @ 28/10/2023
+ * Coded by the founders of Kira-Development-Team
+ * EmpireMTR & Vifez
+ *
+ */
+
 import lombok.Getter;
-import xyz.kiradev.clash.Clash;
+import org.bukkit.plugin.java.JavaPlugin;
 import xyz.kiradev.exler.loader.ExlerLoader;
 
+import java.io.File;
+import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Getter
-public final class Exler extends Clash {
+public final class Exler extends JavaPlugin {
+
+    private static final String PRODUCTS_CONFIG_KEY = "Products";
+    private static final String BASE_URL = "kiradev.xyz/loader/get-product/"; // Change to whatever url you want
+    private static final Logger LOGGER = Logger.getLogger(Exler.class.getName());
 
     private ExlerLoader loader;
 
     @Override
     public void onLoad() {
-        setupConfig();
-        loader = new ExlerLoader(this);
-        for (String str : getConfig().getStringList("Products")) {
-            // TODO: Check licenses before loading
-            loader.loadPlugin(str, "kiradev.xyz/loader/get-product/" + str.toLowerCase());
+        try {
+            setupConfig();
+            loader = new ExlerLoader(this);
+            getConfig().getStringList(PRODUCTS_CONFIG_KEY).forEach(this::loadProduct);
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Unable to load the plugin fr u gotta get better bud", e);
         }
+    }
+
+    private void loadProduct(String product) {
+        loader.loadPlugin(product, BASE_URL + product.toLowerCase());
     }
 
     @Override
     public void onDisable() {
-        if(loader.getTempFile() != null) loader.getTempFile().delete();
+        Optional.ofNullable(loader.getTempFile()).ifPresent(File::delete);
+    }
+
+    private void setupConfig() {
+        saveDefaultConfig();
     }
 }
